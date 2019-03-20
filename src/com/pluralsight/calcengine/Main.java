265913c6abf17cb4d06aa5546020ac9030b75dc1
Ryan Manse <ryanmanse@gmail.com>
2019-03-20 18:18:40 -0500

    add expansive datatypes

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -8,13 +8,29 @@ public class Main {
 //        System.out.println("test=");
 //        System.out.println(testEquation.getResult());
 
-        MathEquation[] equations = new MathEquation[4];
-        equations[0] = new MathEquation('d',100.0d, 50.0d);
-        equations[1] = new MathEquation('a',25.0d, 92.0d);
-        equations[2] = new MathEquation('s',225.0d, 17.0d);
-        equations[3] = new MathEquation('m',11.0d, 3.0d);
+        String[] statements = {
+                "divide 100.0 50.0",
+                "add 25.0 92.0",
+                "subtract 225.0 17.0",
+                "multiply 11.0 3.0"
+        };
 
-        for(MathEquation equation : equations) {
+        CalculateHelper helper = new CalculateHelper();
+        for(String statement:statements) {
+            helper.process(statement);
+            System.out.println(helper);
+        }
+
+    }
+
+    static void useMathEquation() {
+        MathEquation[] equations = new MathEquation[4];
+        equations[0] = new MathEquation('d', 100.0d, 50.0d);
+        equations[1] = new MathEquation('a', 25.0d, 92.0d);
+        equations[2] = new MathEquation('s', 225.0d, 17.0d);
+        equations[3] = new MathEquation('m', 11.0d, 3.0d);
+
+        for (MathEquation equation : equations) {
             equation.execute();
             System.out.println("result = ");
             System.out.println(equation.getResult());
@@ -40,11 +56,28 @@ public class Main {
         System.out.println("result=");
         System.out.println(equationOverload.getResult());
 
-        equationOverload.execute((double)leftInt, rightInt);
+        equationOverload.execute((double) leftInt, rightInt);
         System.out.println("result=");
         System.out.println(equationOverload.getResult());
 
-    }
+        System.out.println();
+        System.out.println("Using Inheritance");
+        System.out.println();
 
+        CalculateBase[] calculators = {
+                new Divider(100.D, 50.0d),
+                new Adder(25.0d, 92.0d),
+                new Subtracter(225.0d, 17.0d),
+                new Multiplier(11.0d, 3.0d)
+
+        };
+
+        for (CalculateBase calculator : calculators) {
+            calculator.calculate();
+            System.out.println("result=");
+            System.out.println(calculator.getResult());
+        }
+
+    }
 
 }
